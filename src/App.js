3a81bddb3185b8e27aa8b1c8060e8bccbe05c772
Zach Harris <zachbharris94@gmail.com>
2020-02-10 22:36:23 -0600

    basic landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import { animated, useSpring, config } from 'react-spring'
 
-function App() {
+function App({ className }) {
+  const h1Props = useSpring({
+    transform: 'translateY(0)',
+    opacity: 1,
+    from: { transform: 'translateY(64px)', opacity: 0 },
+    config: config.slow,
+  })
+
+  const pProps = useSpring({ opacity: 1, from: { opacity: 0 }, delay: 1500 })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={className}>
+      <animated.h1 style={h1Props}>Assembly</animated.h1>
+      <animated.p style={pProps}>Coming Soon</animated.p>
     </div>
-  );
+  )
 }
 
-export default App;
+const StyledApp = styled(App)`
+  background-color: #1b1b1b;
+  font-family: 'Open Sans', sans-serif;
+  height: 100vh;
+  width: 100vw;
+  color: white;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  h1 {
+    margin: 0;
+  }
+`
+
+export default StyledApp
